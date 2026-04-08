2ea05ea5253c1880bebb4d97aadaf6d71dd54356
farfromsouls <farfromsouls@gmail.com>
2026-04-08 19:19:49 +0700

    better sql reader algo + little program

--- practice/sql/main.py
+++ practice/sql/main.py
@@ -13,23 +13,37 @@ def get_connection():
     # Не ловлю ошибки т.к. всеравно в docker-compose есть health-check
     return connection
 
-def main():
+def fetchall(line, cursor):
+    print(f"{line}: {cursor.fetchall()}")
+
+def main(script):
     connection = get_connection()
     cursor = connection.cursor()
     exec = cursor.execute
-    fetchall = lambda: print(cursor.fetchall())
     
-    with open("sql_script.txt", "r", encoding="utf-8") as sql_script_file:
-        sql_script_text = sql_script_file.read()
-        sql_commands = sql_script_text.split("\n\n")
-    
-        for command in sql_commands:
-            if command != "":
-                exec(command)
-                if command.startswith("SELECT"):
-                    fetchall()
+    with open(f"{script}.txt", "r", encoding="utf-8") as sql_script_file:
+
+        buffer = ""
+        line_command = 1
+        line_i = 1
+        
+        for line in sql_script_file:
+            if line != "\n":
+                buffer = buffer + line
+            
+            if line.endswith(';\n') or line.endswith(';'):
+                exec(buffer)
+                
+                if buffer.strip().upper().startswith("SELECT"):
+                    fetchall(line_command+1, cursor)
+                    
+                buffer = ""
+                line_command = line_i + 1
+                
+            line_i += 1
+                
 
 if __name__ == "__main__":
-    main()
+    main("fav_food")
     
 
